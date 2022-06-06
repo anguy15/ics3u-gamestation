2b635a9e4f311aa665a05da670c9591087d73ce5
Angn <nguyenandrew137@protonmail.com>
2022-06-06 19:10:11 +0000

    initial framework

---
+++ .ccls-cache/@home@runner@Game-Station-angn137/main.h
@@ -0,0 +1,8 @@
+  //login
+//return if it works
+int makeUser();
+
+//return if it is correct
+int checkPassword(char userID[], char inputStr[]);
+
+char login();
---
+++ main.h
@@ -0,0 +1,9 @@
+  //login
+//return if it works
+int makeUser();
+
+//return if it is correct
+int checkPassword(char userID[], char inputStr[]);
+
+//return if logged in
+int login(char userID[]);
