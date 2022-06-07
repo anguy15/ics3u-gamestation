4dc83d1a9973af91a8fb62d6ed1a02eb6ef193c1
ANDREW NGUYEN <68118210+anguy15@users.noreply.github.com>
2022-06-07 19:48:10 +0000

    Fix winflags breaking game by fixing header files and array was char

--- games/tic-tac-toe/tic-tac-toe.c
+++ games/tic-tac-toe/tic-tac-toe.c
@@ -3,89 +3,144 @@
 void playTicTacToe()
 {
   system("clear");
-  char gameboard[3][3]={0};
-  // reset game board
-  resetGameBoard(gameboard);
+  int gameboard[3][3]={0};//[vertical pos][horizontal pos]
   
   //X: 0,1,2
   //Y: 3,4,5
-  //diagonal: 6,7
+  //diagonal: 6(top left),7(top right)
   int winFlags[2][8]={0};//flags for user wins
   
+  resetGameBoard(gameboard);
   do
   {
     //run a full round
-    for (int i=1; i<=2; i++)
+    for (int i=0; i<2; i++)
     {
       getUserInputs(gameboard, winFlags, i);
+      if (checkWinFlags())
     }
   }while(1);
 }
 
-void getUserInputs(int gameBoard[3][3], int winFlags[2][8], int player)
+static void getUserInputs(int gameBoard[3][3], int winFlags[2][8], int player)
 {
   int userXChoice, userYChoice;
+  
   userErrorFlags userFlags;
-    userFlags.XFlag = 0;
-    userFlags.YFlag = 0;
-    userFlags.InputFlag=0;
+  userFlags.XFlag = 0;
+  userFlags.YFlag = 0;
+  userFlags.InputFlag=0;
+  
   do
   {
     drawGameBoard(gameBoard);
     if (userFlags.InputFlag!=0)
     {
-      printf("Invalid input given, please try again\n\n");
+      printf("Invalid Coordinate given, please try again\n\n");
     }
-    
+
+    //reset flags
     userFlags.XFlag = 0;
     userFlags.YFlag = 0;
     userFlags.InputFlag=0;
 
     //user choices
+    //get column input
     do
     {
+      drawGameBoard(gameBoard);
       if (userFlags.XFlag!=0)
       {
-        printf("Invalid Input, please try again");
+        printf("Invalid Column\n\n");
       }
       userFlags.XFlag=0;
+      
       printf("Which column would you like: ");
       scanf("%i", &userXChoice);
       getchar();
+      
       userFlags.XFlag++;
     }while(userXChoice<1 || userXChoice>3);
     userXChoice--;
 
+    //get row input
     do
     {
+      drawGameBoard(gameBoard);
       if (userFlags.YFlag!=0)
       {
-        printf("Invalid Input, please try again");
+        printf("Invalid Row\n\n");
       }
       userFlags.YFlag=0;
+      
       printf("Which row would you like: ");
       scanf("%i", &userYChoice);
       getchar();
+      
       userFlags.YFlag++;
     }while(userYChoice<1 || userYChoice>3);
     userYChoice--;
     
     userFlags.InputFlag++;
-  }while(gameBoard[userXChoice][userYChoice]!=0);
+  }while(gameBoard[userYChoice][userXChoice]!=0);
   
   //write valid inputs
-  gameBoard[userXChoice][userYChoice]=player;
-  // winFlags[player-1][userXChoice]++;
-  // winFlags[player-1][userYChoice+3]++;
+  gameBoard[userYChoice][userXChoice]=player+1;
+  //setup flags for x and y
+  winFlags[player][userXChoice]++;
+  winFlags[player][userYChoice+3]++;
+  //setup flags from diagonals
+  if ((userYChoice == 0 && userXChoice == 0) || (userYChoice == 2 && userXChoice == 2))
+  {
+    winFlags[player][7]++;//either corner connected with the top right diagonal
+  }
+  else if ((userYChoice == 0 && userXChoice == 2) || (userYChoice == 2 && userXChoice == 0))
+  {
+    winFlags[player][6]++;//either corner connected with the top left diagonal
+  }
+  else if ((userYChoice == 1 && userXChoice == 1))//center placed
+  {
+    winFlags[player][7]++;
+    winFlags[player][6]++;
+  }
+  
 }
 
-void drawGameBoard(int board[3][3])
+static void checkWinFlags(const int winFlags[2][8])
+{
+  for (int x=0; x<2; x++)
+  {
+    for (int y=0; y<8; y++)
+      printf("%i:%i  ",y,winFlags[x][y]);
+    printf("\n");
+  }
+  getchar();
+}
+
+static void printDebugWinFlags(const int winFlags[2][8])
+{
+  for (int x=0; x<2; x++)
+  {
+    for (int y=0; y<8; y++)
+      printf("%i:%i  ",y,winFlags[x][y]);
+    printf("\n");
+  }
+  getchar();
+}
+
+static void drawGameBoard(const int board[3][3])
 {
   system("clear");
-  for (int x=0; x<3; x++)
+  for (int x=2; x>=0; x--)
   {
     for (int y=0; y<3; y++)
     {
+      if (y==0)
+      {
+        //draw row markers
+        printf("%i ", x+1);
+      }
+      
       // print the played piece
       switch (board[x][y])
       {
@@ -105,21 +160,30 @@ void drawGameBoard(int board[3][3])
       {
         printf(" | ");//column separators
       }
-      else if(x!=2)//not the last line
+      else if(x!=0)//not the last line
       {
-        printf("\n");
+        //draw row separators
+        printf("\n  ");
         for (int z=0; z<3; z++)
         {
           printf("---");//row separators
         }
         printf("\n");
       }
+      else//draw column markers
+      {
+        printf("\n  ");
+        for (int z=0; z<3; z++)
+        {
+          printf(" %i ",z+1);//numbers for grid
+        }
+      }
     }
   }
   printf("\n");
 }
 
-void resetGameBoard(int gameBoard[3][3])
+static void resetGameBoard(int gameBoard[3][3])
 {
   for (int x=0; x<3; x++)
   {
--- games/tic-tac-toe/tic-tac-toe.h
+++ games/tic-tac-toe/tic-tac-toe.h
@@ -1,21 +1,38 @@
-#ifndef math_quiz_h
-#define math_quiz_h
+#ifndef tic_tac_toe_h
+#define tic_tac_toe_h
 
 #include <stdio.h>
+#include <stdlib.h>
 
 //variables
 typedef struct
 {
-  int XFlag, YFlag, InputFlag;//flags for user error messages
+  int XFlag;
+  int YFlag;
+  int InputFlag;
 }userErrorFlags;
 
 //  GLOBALS
 void playTicTacToe();
 
 //LOCALS
-void getUserInputs(int gameBoard[3][3], int winFlags[2][8], int player);
-void resetGameBoard(int gameBoard[3][3]);
-void drawGameBoard(int board[3][3]);
-void checkUserPlacement(int board[3][3], int xCoord, int yCoord);
+//gets all inputs for one round, and places them in gameboard
+//winflags are setup depending on the inputs, and player
+static void getUserInputs(int gameBoard[3][3], int winFlags[2][8], int player);
+
+//resets the board to 0
+static void resetGameBoard(int gameBoard[3][3]);
+
+//draws the current game board
+static void drawGameBoard(const int board[3][3]);
+
+//check if the win flags are finished
+static void checkWinFlags(const int winFlags[2][8])
+
+//a debug function which prints the values of win flags
+static void printDebugWinFlags(const int winFlags[2][8]);
+
+//checks if the coordinate given by inputs of the player are valid
+static void checkUserPlacement(int board[3][3], int xCoord, int yCoord);
 
 #endif
